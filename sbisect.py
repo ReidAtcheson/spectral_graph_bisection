b8a4e746a2fedab49f539ddacdbb47c4ec167ad5
Reid Atcheson <reid.atcheson@gmail.com>
2022-09-05 15:08:37 -0500

    added bisection

--- sbisect.py
+++ sbisect.py
@@ -7,8 +7,11 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 
-
+#Computes graph laplacian for sparsity structure of input
+#sparse matrix
 def graph_laplacian(A):
+    #Symmetrize the input graph
+    A=A+A.T
     m,n=A.shape
 
     rs=[]
@@ -30,6 +33,75 @@ def graph_laplacian(A):
         cs.append(r)
         vals.append(deg)
     return sp.coo_matrix((vals,(rs,cs)))
+
+
+#Computes fiedler vector via inverse iteration using
+#conjugate gradients as underlying solver
+#Each step of iteration we project out the nullspace of G
+
+def fiedler(G,tol=1e-6,maxiter=20,seed=42,verbosity=0):
+    m,n=G.shape
+    assert(m==n)
+    rng=np.random.default_rng(seed)
+    k=5
+    X=rng.uniform(-1,1,size=(m,k))
+    w,V=spla.lobpcg(G,X,Y=(np.ones(m)/np.sqrt(m)).reshape((m,1)),tol=tol,maxiter=maxiter,largest=False,verbosityLevel=verbosity)
+    ids=np.argsort(w)
+    w=w[ids]
+    V=V[:,ids]
+    return w[0],V[:,0]
+
+
+
+#Uses fiedler vector to compute a 3-way partition [b0,s,b1] where s is a separator
+#between b0 and b1
+def split(G,tol=1e-6,maxiter=20,seed=42,verbosity=0):
+    G=sp.csr_matrix(G)
+    m,n=G.shape
+    assert(m==n)
+    t,f=fiedler(G,tol=tol,maxiter=maxiter,seed=seed,verbosity=verbosity)
+    f=f/np.linalg.norm(f,ord=np.inf)
+    b0=set(filter(lambda i : f[i]<0.0,range(0,m)))
+    b1=set(filter(lambda i : f[i]>=0.0,range(0,m)))
+    s0=set()
+    s1=set()
+    for i in b0:
+        cids=G.indices
+        offs=G.indptr
+        beg,end=offs[i],offs[i+1]
+        for c in cids[beg:end]:
+            if c in b1:
+                s0.add(c)
+
+    b1 = b1.difference(s0)
+    for i in b1:
+        cids=G.indices
+        offs=G.indptr
+        beg,end=offs[i],offs[i+1]
+        for c in cids[beg:end]:
+            if c in b0:
+                s1.add(c)
+
+
+    b0 = b0.difference(s1)
+    s = s0.union(s1)
+
+
+    return (list(b0),list(s),list(b1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -47,6 +47,91 @@ class TestSbisect(unittest.TestCase):
         self.assertLess(la.norm(Ge),1e-14)
 
 
+    #Graph laplacian should be symmetric
+    def test_graph_laplacian_symmetric(self):
+        seed=2398743
+        rng=np.random.default_rng(seed)
+        ndiags=10
+        m=256
+        offs = np.sort(rng.choice(list(range(-m+1,0))+list(range(1,m)),size=ndiags,replace=False))
+        A=sp.diags([rng.uniform(-1,1,m) for _ in range(ndiags)],offs,shape=(m,m)) + sp.diags([rng.uniform(-1,1,m)],[0],shape=(m,m))
+        G=sbisect.graph_laplacian(A)
+
+        self.assertLess( sp.linalg.norm(G-G.T), 1e-14 )
+
+
+
+
+
+
+
+    #Compare "matrix-free" fiedler vector calculation
+    #to dense eigenvalue calculation
+    def test_fiedler_vector(self):
+        seed=2398743
+        rng=np.random.default_rng(seed)
+        ndiags=5
+        m=256
+        offs = [-32,-1,0,1,32]
+        A=sp.diags([rng.uniform(-1,1,m) for _ in range(ndiags)],offs,shape=(m,m)) 
+        G=sbisect.graph_laplacian(A)
+        (t,f)=sbisect.fiedler(G,tol=1e-4,maxiter=200,verbosity=-1)
+
+        es,V = la.eigh(G.toarray())
+
+        self.assertLess( abs(es[1]-t)/es[1], 1e-4 )
+
+    def test_split_partition(self):
+        seed=2398743
+        rng=np.random.default_rng(seed)
+        ndiags=5
+        m=256
+        offs = [-32,-1,0,1,32]
+        A=sp.diags([rng.uniform(-1,1,m) for _ in range(ndiags)],offs,shape=(m,m)) 
+        G=sbisect.graph_laplacian(A)
+
+        b0,s,b1 = sbisect.split(G)
+
+        #Check this is a partition
+        self.assertEqual(sorted(b0+s+b1),list(range(0,m)))
+
+    def test_split_separator(self):
+        seed=2398743
+        rng=np.random.default_rng(seed)
+        ndiags=5
+        m=256
+        offs = [-32,-1,0,1,32]
+        A=sp.diags([rng.uniform(-1,1,m) for _ in range(ndiags)],offs,shape=(m,m)) 
+        G=sbisect.graph_laplacian(A)
+
+        b0,s,b1 = sbisect.split(G)
+
+        #Check that `s` separates `b0` and `b1`
+        x=np.zeros(m)
+        x[b0]=rng.uniform(-1,1,size=len(b0))
+        y=G@x
+        self.assertEqual(list(y[b1]),list(np.zeros(len(b1))))
+
+        x=np.zeros(m)
+        x[b1]=rng.uniform(-1,1,size=len(b1))
+        y=G@x
+        self.assertEqual(list(y[b0]),list(np.zeros(len(b0))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
